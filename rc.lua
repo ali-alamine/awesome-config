45c7e820d3d95ff37e1a2e18c2728ef4bd2453e2
Ali <ali@localhost.localdomain>
2014-11-22 01:32:17 +0200

    Modified the battery widget to show as a progress bar instead of text

--- rc.lua
+++ rc.lua
@@ -116,28 +116,32 @@
 
 -- {{{ Wibox
 	-- Create the battery widget
-	mybatterywidget = wibox.widget.textbox()
+	mybatterywidget = awful.widget.progressbar()
+	mybatterywidget:set_width(8)
+	mybatterywidget:set_height(10)
+	mybatterywidget:set_vertical(true)
+	mybatterywidget:set_background_color('#000000')
+	mybatterywidget:set_color('FFFFFF')
+	mybatterywidget:set_value(0)
 	bw_timer = timer({ timeout = 1 })
 	bw_timer:connect_signal("timeout", function()
-		local textToShow = ""
-		local fd = io.popen("upower -d | awk '$1=/percentage/{print $2;exit;}'")
+		--local textToShow = ""
+		local fd = io.popen("upower -d | awk '$1=/percentage/{print $2;exit;}' | sed -e 's/^\\([[:digit:]]*\\)\\(%\\)$/\\1/g'")
 		local str = fd:read("*l")
 		io.close(fd)
 		fd = io.popen("upower -d | awk '/state/{print $2;exit}'")
 		local state = fd:read("*l")
 		io.close(fd)
-		fd = io.popen("upower -d | awk '/time to empty/{print $4; exit;}'")
-		local time = fd:read("*l")
-		io.close(fd)
 		if not time then
-			textToShow = str
+			textToShow = ''
 		else
-			textToShow = str .. " remaining:" .. time .. "h"
+			textToShow = " remaining:" .. time .. "h"
 		end
+		mybatterywidget:set_value(tonumber(str) / 100)
 		if state == "charging" then
-			mybatterywidget:set_markup("<span fgcolor='yellow'>" .. textToShow .. "</span>")
+			mybatterywidget:set_color('#00AB1F')
 		else
-			mybatterywidget:set_markup("<span fgcolor='red'>" .. textToShow.. "</span>")
+			mybatterywidget:set_color('#DB0000')
 		end
 	end)
 	bw_timer:start()
