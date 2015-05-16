cc9951b4716b22e872381e396f83ba1b0fc19ea9
Ali Al Amine <alialamine95@gmail.com>
2015-05-16 13:30:17 +0300

    The applications now start only if not already running

--- ApplicationStartup.lua
+++ ApplicationStartup.lua
@@ -1,16 +1,31 @@
 local awful = require("awful")
 
+-- This function finds if a process is running, if not, runs it
+-- It also checks for python scripts, java apps, etc.
+function runOnce(appName, processName)
+	if processName == nil then
+		processName = appName
+	end
+	local fd = io.popen(
+						"ps ax | grep -v grep | grep " .. processName)
+	local pid = fd:read("*all")
+	io.close(fd)
+	if pid == nil or pid == '' then
+		awful.util.spawn_with_shell(appName)
+	end
+end
+
 -- Screen Saver
-awful.util.spawn_with_shell("/usr/bin/xscreensaver -no-splash &")
+runOnce("/usr/bin/xscreensaver -no-splash", "xscreensaver")
 
 -- Browser
-awful.util.spawn_with_shell("google-chrome-unstable &")
+runOnce("google-chrome-unstable", "chrome")
 
 -- Music Player
-awful.util.spawn_with_shell("vlc &")
+runOnce("vlc", "vlc")
 
 -- Clipboard manager
-awful.util.spawn_with_shell("clipit &")
+runOnce("clipit")
 
 -- Network manager
-awful.util.spawn_with_shell("wicd-gtk &")
+runOnce("wicd-gtk", "wicd-client")
