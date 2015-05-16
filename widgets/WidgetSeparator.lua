355ea58c77260b158ec7aa3c79d9c1b60a26a73b
Ali Al Amine <alialamine95@gmail.com>
2015-05-16 14:37:36 +0300

    Removed useless commented code

--- widgets/WidgetSeparator.lua
+++ widgets/WidgetSeparator.lua
@@ -1,6 +1,4 @@
 	local wibox = require("wibox")
 -- Create the separator widget
---separator_widget= wibox.widget.imagebox()
---separator_widget:set_image(awful.util.getdir("config") .. "/vertical_separator.png")
 separator_widget = wibox.widget.textbox()
 separator_widget:set_text("  |  ")
