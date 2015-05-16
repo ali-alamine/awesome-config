f0e0dd87428cbbfa0e1e05c862a1eaa00ff0d420
Ali Al Amine <alialamine95@gmail.com>
2015-05-16 15:18:13 +0300

    Modified the battery bar to use vicious and added some features

--- widgets/BatteryBar.lua
+++ widgets/BatteryBar.lua
@@ -1,36 +1,35 @@
 local awful = require("awful")
 local vicious = require("vicious")
+local naughty = require("naughty")
 
 -- Create the battery widget
 mybatterywidget = awful.widget.progressbar()
 mybatterywidget:set_width(8)
 mybatterywidget:set_height(10)
 mybatterywidget:set_vertical(true)
-mybatterywidget:set_background_color('#000000')
+mybatterywidget:set_background_color('000000')
 mybatterywidget:set_color('FFFFFF')
 mybatterywidget:set_value(0)
---vicious.register( mybatterywidget, vicious.widgets.bat, "$2", 1, "BAT")
-local bw_timer = timer({ timeout = 1 })
-bw_timer:connect_signal("timeout", function()
-	local fd = io.popen(
-				 "upower -d "
-			.. "| awk '$1=/percentage/{print $2;exit;}' "
-			.. "| sed -e 's/^\\([[:digit:]]*\\)\\(%\\)$/\\1/g'")
-	local str = fd:read("*l")
-	io.close(fd)
-	fd = io.popen("upower -d | awk '/state/{print $2;exit}'")
-	local state = fd:read("*l")
-	io.close(fd)
-	if not time then
-		textToShow = ''
-	else
-		textToShow = " remaining:" .. time .. "h"
-	end
-	mybatterywidget:set_value(tonumber(str) / 100)
-	if state == "charging" then
-		mybatterywidget:set_color('#00AB1F')
-	else
-		mybatterywidget:set_color('#DB0000')
-	end
-end)
-bw_timer:start()
+vicious.register( mybatterywidget, vicious.widgets.bat,
+	function(widget, args)
+		if args[1] == '−' then
+			-- Battery is discharging
+			widget:set_color("DB0000")
+		elseif args[1] == '+' then
+			-- Battery is charging
+			widget:set_color("00AB1F")
+		else
+			-- None of the above
+			widget:set_color("0000FF")
+		end
+		if args[1] == '−' and args[2] < 10 then
+			naughty.notify({ preset = naughty.config.presets.notice,
+					 title = "Battery Level is Low",
+					 text = args[2],
+					 timeout = 0.997})
+		end
+		--naughty.notify({ preset = naughty.config.presets.notice,
+				 --title = "Battery",
+				 --text = args[1] })
+		return args[2]
+	end, 1, "BAT1")
