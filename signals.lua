1b4b84f75f5c4195381828785486b05be3c53f02
Ali Al Amine <alialamine95@gmail.com>
2015-05-20 12:35:53 +0300

    Added a signal to close google chrome peacefuly

--- signals.lua
+++ signals.lua
@@ -71,4 +71,11 @@ local awful = require("awful")
 
 	client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 	client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
+	client.connect_signal("exit", function(restart)
+		if not restart then
+			-- Close google chrome peacefully
+			local fd = io.popen("pkill -o chrome")
+			io.close(fd)
+		end
+	end)
 -- }}}
