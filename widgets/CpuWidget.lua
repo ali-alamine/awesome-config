bc9f6b1e84259021fef88d57f25f4a94a56c7ed1
Ali Al Amine <alialamine95@gmail.com>
2015-05-20 12:38:17 +0300

    Added labels for the cpu and volume widgets

--- widgets/CpuWidget.lua
+++ widgets/CpuWidget.lua
@@ -1,12 +1,21 @@
 local awful = require("awful")
 local vicious = require("vicious")
+local wibox = require("wibox")
 
 -- Creating a cpu graph
 
-cpuwidget = awful.widget.graph()
-cpuwidget:set_width(50)
-cpuwidget:set_background_color("#494B4F")
-cpuwidget:set_color({ type = "linear", from = { 0, 0 }, to = { 50, 0 },
+local cpuwidgetGraph = awful.widget.graph()
+cpuwidgetGraph:set_width(50)
+cpuwidgetGraph:set_background_color("#494B4F")
+cpuwidgetGraph:set_color({ type = "linear", from = { 0, 0 }, to = { 50, 0 },
 		stops = { { 0, "#FF5656" }, { 0.5, "#88A175" }, { 1, "#AECF96" }}})
 vicious.cache(vicious.widgets.cpu)
-vicious.register(cpuwidget, vicious.widgets.cpu, "$1", 1)
+vicious.register(cpuwidgetGraph, vicious.widgets.cpu, "$1", 1)
+
+
+local cpuWidgetLabel = wibox.widget.textbox()
+cpuWidgetLabel:set_text("CPU:")
+
+cpuwidget = wibox.layout.fixed.horizontal()
+cpuwidget:add(cpuWidgetLabel)
+cpuwidget:add(cpuwidgetGraph)
--- widgets/VolumeBar.lua
+++ widgets/VolumeBar.lua
@@ -1,10 +1,18 @@
 local blingbling = require("blingbling")
+local wibox = require("wibox")
 
-my_volume=blingbling.volume.new()
-my_volume:set_height(18)
-my_volume:set_width(30)
-my_volume:set_graph_color("#449911")
+local volume=blingbling.volume.new()
+volume:set_height(18)
+volume:set_width(30)
+volume:set_graph_color("#449911")
 --bind the volume widget on the master channel
-my_volume:update_master()
-my_volume:set_master_control()
-my_volume:set_bar(true)
+volume:update_master()
+volume:set_master_control()
+volume:set_bar(true)
+
+local volume_label = wibox.widget.textbox()
+volume_label:set_text("Vol:")
+
+volume_widget = wibox.layout.fixed.horizontal()
+volume_widget:add(volume_label)
+volume_widget:add(volume)
